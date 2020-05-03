453885347b4b4feb94cbe56524bce64f31bcafef
Carla Morral <carla.morral@gmail.com>
2020-05-03 14:32:57 +0200

    React animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -109,7 +109,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     const c = comments.map((comment) => {
         return(
             <li key={comment.id}>
@@ -126,7 +126,7 @@ function RenderComments({comments, addComment, dishId}) {
             <ul className="list-unstyled">
                 {c}
             </ul>
-            <CommentForm dishId={dishId} addComment={addComment}/>
+            <CommentForm dishId={dishId} postComment={postComment}/>
         </div>
     )
 }
@@ -166,7 +166,7 @@ const Dishdetail = (props) => {
                     <div className="row">
                         <RenderDish dish={props.dish}/>
                         <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id}/>
                     </div>
             </div>
